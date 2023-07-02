23875794c73d659e73eed92587660cf523d9f5d7
brynard <brynard.bryan@vimigotech.com>
2023-07-02 13:41:09 +0800

    add perfect scrollbar in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const isProduction = mode === 'production';
+
+    return {
+        plugins: [
+            laravel({
+                input: [
+                    'resources/sass/app.scss',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+        build: {
+            rollupOptions: {
+                external: isProduction ? ['perfect-scrollbar'] : [],
+            },
+        },
+    };
 });
